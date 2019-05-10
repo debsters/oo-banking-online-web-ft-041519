4ed615168ab987bde61988c315023b6152c4590b
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-10 20:27:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  attr_accessor :status, :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 end
